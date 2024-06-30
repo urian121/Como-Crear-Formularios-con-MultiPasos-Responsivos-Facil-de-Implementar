5471e8d084a61aa82d79fc49e40cc3cc1aa9793d
URIAN VIERA <urian1213viera@gmail.com>
2024-06-30 10:55:03 -0500

    Add files via upload

---
+++ multi-steps-form/home.js
@@ -0,0 +1,32 @@
+var form1 = document.getElementById("form1"),
+  form2 = document.getElementById("form2"),
+  form3 = document.getElementById("form3");
+
+var next1 = document.getElementById("next1"),
+  next2 = document.getElementById("next2"),
+  back1 = document.getElementById("back1"),
+  back2 = document.getElementById("back2");
+
+var progress = document.getElementById("progress");
+
+next1.onclick = function () {
+  form1.style.left = "-450px";
+  form2.style.left = "40px";
+  progress.style.width = "240px";
+};
+back1.onclick = function () {
+  form1.style.left = "40px";
+  form2.style.left = "450px";
+  progress.style.width = "120px";
+};
+
+next2.onclick = function () {
+  form2.style.left = "-450px";
+  form3.style.left = "40px";
+  progress.style.width = "360px";
+};
+back2.onclick = function () {
+  form2.style.left = "40px";
+  form3.style.left = "450px";
+  progress.style.width = "240px";
+};
